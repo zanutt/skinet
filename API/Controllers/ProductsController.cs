34a5b02e802ab961f6bbe3e5c775bc8e6e3cda33
zanutt <matszg@live.com>
2024-12-27 02:57:06 -0300

    Initial Commit2.0

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,14 +13,22 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
-        public string GetProducts(){
-            return "This will be a list of products";
+        public async Task<ActionResult<List<ProductsController>>> GetProducts(){
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
         }
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "This will be a single product";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,7 +1,8 @@
 using Microsoft.EntityFrameworkCore;
-using API.Entities;
+using Core.Entities;
 
-namespace API.Data
+
+namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
